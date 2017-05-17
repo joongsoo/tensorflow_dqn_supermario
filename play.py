4f220d06c89dbe9a356f34d4dd34ca062db03199
Daniel <wndtn853@gmail.com>
2017-05-17 22:52:19 +0900

    apply network

--- play.py
+++ play.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 import numpy as np
-import random
-import dqn
 from collections import deque
 from data.env import Env
-import multiprocessing
-from multiprocessing import Manager
-import time
-
+from tensorflow.python.framework.errors_impl import NotFoundError
 
 class AIControl:
     def __init__(self, env):
@@ -25,32 +20,50 @@ class AIControl:
         self.save_path = "./save/save_model"
 
     def control_start(self):
+        import dqn
         with tf.Session() as sess:
-            mainDQN = dqn.DQN(sess, self.input_size, self.output_size, name="main")
+            mainDQN = [
+                dqn.DQN(sess, self.input_size, self.output_size, name="main0"),
+                dqn.DQN(sess, self.input_size, self.output_size, name="main1"),
+                dqn.DQN(sess, self.input_size, self.output_size, name="main2"),
+                dqn.DQN(sess, self.input_size, self.output_size, name="main3"),
+                dqn.DQN(sess, self.input_size, self.output_size, name="main4"),
+                dqn.DQN(sess, self.input_size, self.output_size, name="main5")
+            ]
+
+
             tf.global_variables_initializer().run()
 
-            mainDQN.restore()
+            try:
+                for main in mainDQN:
+                    main.restore()
+            except NotFoundError:
+                pass
 
-            for episode in range(40, self.max_episodes):
+
+            for episode in range(1000, self.max_episodes):
                 done = False
                 step_count = 0
                 state = self.env.reset()
                 max_x = 0
 
                 while not done:
-                    action = np.argmax(mainDQN.predict(state))
-                    print action
-                    max_x = self.env.step_play(action)
-                    step_count += 1
+                    action = []
+                    for dqn in mainDQN:
+                        pre = np.argmax(dqn.predict(state))
+
+                        action.append(pre)
+
+                    next_state, reward, done, max_x = self.env.step(action)
+
+                    state = next_state
 
                 print("Episode: {}  steps: {}  max_x: {}".format(episode, step_count, max_x))
-
 
 def main():
     env = Env()
     controller = AIControl(env)
     controller.control_start()
-
 
 if __name__ == "__main__":
     main()
