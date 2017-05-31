cdfc1c134018123fc42f0e41d406c28bfc770d0c
Daniel <wndtn853@gmail.com>
2017-06-01 01:50:27 +0900

    memory flush

--- main_async.py
+++ main_async.py
@@ -197,7 +197,7 @@ class AIControl:
                 # 샘플링 하기에 작은 사이즈는 트레이닝 시키지 않는다
                 if episode % 2 == 0 and len(self.replay_buffer) > 50:
                     self.episode_buffer.append((self.replay_buffer, episode, step_count, max_x, reward_sum))
-                    if len(self.episode_buffer) > 3:
+                    if len(self.episode_buffer) > 10:
                         print 'buffer flush... plz wait...'
                         while len(self.episode_buffer) != 0:
                             time.sleep(1)
