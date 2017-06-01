4f720cef04980ce2c01a3dd7c662bdb491dcc50d
Daniel <wndtn853@gmail.com>
2017-06-01 09:47:24 +0900

    add frame action rate

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ class AIControl:
                     fp.write(str(input_list))
 
                 # 샘플링 하기에 작은 사이즈는 트레이닝 시키지 않는다
-                if len(self.replay_buffer) > 50:
+                if episode % 2 == 0 and len(self.replay_buffer) > 50:
                     self.episode_buffer.append((self.replay_buffer, episode, step_count, max_x, reward_sum))
                     if len(self.episode_buffer) > 0:
                         print 'buffer flush... plz wait...'
