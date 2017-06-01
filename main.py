efb3e37f0ac015b5e7f3895a273213d65c66c252
Daniel <wndtn853@gmail.com>
2017-06-01 09:46:58 +0900

    memory flush

--- main.py
+++ main.py
@@ -186,12 +186,12 @@ class AIControl:
                     fp.write(str(input_list))
 
                 # 샘플링 하기에 작은 사이즈는 트레이닝 시키지 않는다
-                if episode % 2 == 0 and len(self.replay_buffer) > 50:
+                if len(self.replay_buffer) > 50:
+                    self.episode_buffer.append((self.replay_buffer, episode, step_count, max_x, reward_sum))
                     if len(self.episode_buffer) > 0:
                         print 'buffer flush... plz wait...'
                         while len(self.episode_buffer) != 0:
                             time.sleep(1)
-                    self.episode_buffer.append((self.replay_buffer, episode, step_count, max_x, reward_sum))
                     self.replay_buffer = deque()
 
                 episode += 1
