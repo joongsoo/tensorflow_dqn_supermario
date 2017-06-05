8850f0bd3e341094e3acc7f3edf66389c467071e
Daniel <wndtn853@gmail.com>
2017-06-05 16:30:36 +0900

    memory flush

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class AIControl:
         self.output_size = 12
 
         #self.dis = 0.9
-        self.dis = 0.7
+        self.dis = 0.9
         self.val = 0
         self.save_path = "./save/save_model"
 
