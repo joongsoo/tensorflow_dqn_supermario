29e8de342e97b1c9a95a2092ca02734bf76f0687
Daniel <wndtn853@gmail.com>
2017-06-08 22:44:25 +0900

    memory flush

--- dqn.py
+++ dqn.py
@@ -62,17 +62,6 @@ class DQN:
                 1, 2, 2, 1], padding='SAME')
             L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 
-            # L3 ImgIn shape=(?, 7, 7, 64)
-            W3 = tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01))
-            #    Conv      ->(?, 7, 7, 128)
-            #    Pool      ->(?, 4, 4, 128)
-            #    Reshape   ->(?, 4 * 4 * 128) # Flatten them for FC
-            L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
-            L3 = tf.nn.relu(L3)
-            L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[
-                1, 2, 2, 1], padding='SAME')
-            L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
-
             L3 = tf.reshape(L3, [-1, 128 * 15 * 15])
 
             # L4 FC 4x4x128 inputs -> 625 outputs
