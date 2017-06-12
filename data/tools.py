5af4c1c4e2165a81ad8f8045b434010afc3c2935
Daniel <wndtn853@gmail.com>
2017-06-12 14:50:45 +0900

    memory flush

--- data/tools.py
+++ data/tools.py
@@ -88,7 +88,7 @@ class Control(object):
         # time penalty
         #reward -= 0.1
         if self.keys[275] == 1:
-            reward += 0.5
+            reward += 1
 
         '''
         if self.keys[276] == 1:
