dce9532547fd11a45e21c70d96ab0c9379aa033f
Daniel <wndtn853@gmail.com>
2017-06-14 17:58:23 +0900

    memory flush

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class AIControl:
     def async_training(self, sess, ops, ops_temp):
         step = 0
         epoch = 30
-        batch_size = 250
+        batch_size = 1000
         while self.training:
             if len(self.episode_buffer) > 0:
                 replay_buffer, episode, step_count, max_x, reward_sum = self.episode_buffer.popleft()
@@ -241,7 +241,7 @@ class AIControl:
                 '''
 
                 #if len(self.replay_buffer) > self.MAX_BUFFER_SIZE:
-                if episode % 5 == 0 and len(self.replay_buffer) > 100:
+                if episode % 10 == 0 and len(self.replay_buffer) > 100:
                     self.episode_buffer.append((self.replay_buffer, episode, step_count, max_x, reward_sum))
                     if len(self.episode_buffer) > 0:
                         print 'buffer flush... plz wait...'
