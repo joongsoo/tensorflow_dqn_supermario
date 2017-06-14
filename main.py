52f6457b70a7c02b0a85c222469ca1f446aecf97
Daniel <wndtn853@gmail.com>
2017-06-14 12:11:40 +0900

    memory flush

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ class AIControl:
             episode = 0
             #REPLAY_MEMORY = self.get_memory_size(episode)
             while episode < self.max_episodes:
-                e = max(0.1, min(0.5, 1. / ((episode / 100) + 1)))
+                e = max(0.1, min(0.5, 1. / ((episode / 200) + 1)))
                 #
                 done = False
                 clear = False
