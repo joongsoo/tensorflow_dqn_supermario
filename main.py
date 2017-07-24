6e24600af596d8f6ac44d6a5120dc2ae93d217c1
Daniel <wndtn853@gmail.com>
2017-07-24 17:32:32 +0900

    memory flush

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class AIControl:
 
         self.frame_action = 3
         self.training = True
-        self.step = 6301
+        self.step = 8351
 
 
     def async_training(self, sess, ops):
@@ -99,7 +99,7 @@ class AIControl:
             #self.tempDQN = dqn.DQN(sess, self.input_size, self.output_size, name="temp")
             tf.global_variables_initializer().run()
 
-            episode = 6300
+            episode = 8350
             best_x = 0
             try:
                 self.mainDQN.restore(episode)
@@ -119,9 +119,9 @@ class AIControl:
 
             start_position = 500
 
-            episode = 102806
+            episode = 141500
             while episode < self.max_episodes:
-                e = max(0.05, min(0.75, 1 / ((self.step / 1000) + 0.2)))
+                e = 0.2#max(0.05, min(0.75, 1 / ((self.step / 1000) + 0.2)))
                 #max(0.05, min(0.3, 1. / ((episode / 5000) + 1)))
 
                 done = False
